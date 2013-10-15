8a4d39bfbfd222fea3b4370d9a121c0780cb5481
Mantas Mikulėnas <grawity@gmail.com>
2013-10-15 15:25:25 +0300

    mutt: clean up

--- mail/muttrc.sh
+++ mail/muttrc.sh
@@ -1,29 +1,24 @@
 #!/bin/bash
 
-#[[ -x /usr/sbin/sendmail || -x /usr/lib/sendmail ]] &&
-#cat <<-'!'
-#	set smtp_url="smtp://grawity@equal.cluenet.org"
-#!
-
 [[ -f ~/.msmtprc ]] &&
-cat <<-'!'
-	set sendmail="msmtp"
+cat <<!
+set sendmail="msmtp"
 !
 
 [[ "$GPG_AGENT_INFO" ]] &&
-cat <<-'!'
-	set crypt_use_gpgme
+cat <<!
+set crypt_use_gpgme
 !
 
 [[ -f "/usr/share/doc/mutt/README.Debian" ]] &&
-cat <<-'!'
-	set xterm_set_titles
+cat <<!
+set xterm_set_titles
 !
 
 [[ -d ~/.cache/mutt ]] &&
-cat <<-'!'
-	set header_cache="~/.cache/mutt"
-	set message_cachedir="~/.cache/mutt"
+cat <<!
+set header_cache="~/.cache/mutt"
+set message_cachedir="~/.cache/mutt"
 !
 
 [[ -f ~/.muttrc-"$HOSTNAME" ]] &&
