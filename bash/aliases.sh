43bdecb7fd0166569a17429b59e3b8bda75edaa8
Mantas Mikulėnas <grawity@gmail.com>
2013-10-30 20:59:00 +0200

    bash: aliases: fix _thiscommand for huge history (10000+th command)

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -85,7 +85,7 @@ splitext() { split -dC "${2-32K}" "$1" "${1%.*}-" --additional-suffix=".${1##*.}
 alias srs='rsync -vhzaHAX'
 alias sudo='sudo ' # for alias expansion in sudo args
 alias takeown='sudo chown "${UID}:${GROUPS[0]}"'
-_thiscommand() { history 1 | sed "s/^\s[0-9]\+\s\+$1\s\+//"; }
+_thiscommand() { history 1 | sed "s/^\s*[0-9]\+\s\+$1\s\+//"; }
 alias tidiff='infocmp -Ld'
 alias todo:='todo "$(_thiscommand todo:)" #'
 alias tracert='traceroute'
