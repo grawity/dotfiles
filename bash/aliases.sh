f5c9ebdf55065cfbd9d9f17bd37b00f8028d0ab5
Mantas Mikulėnas <grawity@gmail.com>
2014-02-21 22:52:43 +0200

    bash: aliases: like `look` but with a tree

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -46,7 +46,8 @@ alias logoff='logout'
 if [[ $DESKTOP_SESSION ]]; then
 	alias logout='~/code/x11/logout'
 fi
-look() { find -iname "*$1*" "${@:2}"; }
+look() { find . -iname "*$1*" "${@:2}"; }
+f() { find "$PWD" -iname "*$1*" "${@:2}" | treeify "$PWD"; }
 alias lp='sudo netstat -lptu --numeric-hosts'
 alias lpt='sudo netstat -lpt --numeric-hosts'
 alias lpu='sudo netstat -lpu --numeric-hosts'
