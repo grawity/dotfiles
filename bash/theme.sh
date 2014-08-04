f552586108cecb18dca301616a2fba1df421c027
Mantas Mikulėnas <grawity@gmail.com>
2014-08-04 23:35:10 +0300

    bash/theme: drop 256-color checks
    Some of them were half-done (e.g. some items checked, some not), and
    others are no longer neccessary as the Linux console finally understands
    the 256-color codes (though it still maps them to 16 colors).

--- bash/theme.sh
+++ bash/theme.sh
@@ -44,14 +44,9 @@ fi
 case $FQDN in
     rain.nullroute.eu.org)
 	fmt_name_self='38;5;82'
-	if (( havecolor == 256 )); then
-		fmt_pwd='38;5;39'
-		fmt_pwd_tail='38;5;45'
-		fmt_vcs='38;5;202'
-	else
-		fmt_pwd='33'
-		fmt_vcs='1;31'
-	fi
+	fmt_pwd='38;5;39'
+	fmt_pwd_tail='38;5;45'
+	fmt_vcs='38;5;202'
 	;;
 
     *.nullroute.eu.org)
@@ -73,15 +68,10 @@ case $FQDN in
 	;;
 
     *.core|*.rune)
-	if (( havecolor == 256 )); then
-		item_name_pfx="["
-		item_name=$FQDN
-		item_name_sfx="] $OSTYPE"
-		fmt_name_pfx='|38;5;242'
-	else
-		item_name=$FQDN
-		item_name_sfx=" $OSTYPE"
-	fi
+	item_name_pfx="["
+	item_name=$FQDN
+	item_name_sfx="] $OSTYPE"
+	fmt_name_pfx='|38;5;242'
 	if (( UID )); then
 		fmt_name='38;5;71'
 	else
@@ -97,26 +87,14 @@ case $FQDN in
 	item_name=$FQDN
 	fullpwd=y
 	item_prompt='└'
-
-	if (( havecolor == 256 )); then
-		if (( UID )); then
-			fmt_name='38;5;31'
-		else
-			fmt_name_pfx='38;5;196'
-			fmt_name='|48;5;196'
-		fi
-		fmt_pwd='38;5;76'
-		fmt_vcs='38;5;198'
+	if (( UID )); then
+		fmt_name='38;5;31'
 	else
-		if (( UID )); then
-			fmt_name='36'
-		else
-			fmt_name='1;31'
-		fi
-		fmt_pwd='32'
-		fmt_vcs='1;35'
+		fmt_name_pfx='38;5;196'
+		fmt_name='|48;5;196'
 	fi
-
+	fmt_pwd='38;5;76'
+	fmt_vcs='38;5;198'
 	: ${fmt_name_pfx:=$fmt_name}
 	fmt_prompt=$fmt_name_pfx
 esac
