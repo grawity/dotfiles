b65335254858a9d77b1cf459b893400311fe1550
Mantas Mikulėnas <grawity@gmail.com>
2014-10-09 21:08:42 +0300

    bash/aliases: ICMP traceroute

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -93,7 +93,7 @@ _thiscommand() { history 1 | sed "s/^\s*[0-9]\+\s\+$1\s\+//"; }
 alias tidiff='infocmp -Ld'
 alias todo:='todo "$(_thiscommand todo:)" #'
 alias traceroute='traceroute -e -A'
-alias tracert='traceroute'
+alias tracert='traceroute -I'
 alias treedu='tree --du -h'
 up() { local p i=${1-1}; while ((i--)); do p+=../; done; cd "$p$2" && pwd; }
 vercmp() {
