8c5e1cbe0a3cc07a1e84be7601bd9a153c4e206c
Mantas Mikulėnas <grawity@gmail.com>
2015-06-15 16:28:09 +0300

    dotfiles, bash/aliases: get rid of .xsel.log

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -169,10 +169,10 @@ if have xclip; then
 	alias pclip='xclip -out -selection clipboard'
 	alias gclip='xclip -in -selection clipboard'
 elif have xsel; then
-	alias psel='xsel -o -p'
-	alias gsel='xsel -i -p'
-	alias pclip='xsel -o -b'
-	alias gclip='xsel -i -b'
+	alias psel='xsel -o -p -l/dev/null'
+	alias gsel='xsel -i -p -l/dev/null'
+	alias pclip='xsel -o -b -l/dev/null'
+	alias gclip='xsel -i -b -l/dev/null'
 fi
 
 clip() {
