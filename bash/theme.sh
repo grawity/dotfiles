50f8cab8cc76e4905f17d5f9267ea214c0066a52
Mantas Mikulėnas <grawity@gmail.com>
2015-07-12 02:40:48 +0300

    bash/prompt: port all remaining themes

--- bash/theme.sh
+++ bash/theme.sh
@@ -1,12 +1,5 @@
 # bashrc -- prompt appearance (color variables)
 
-# Prompt layout:
-#
-#   {item_name}{reset_pwd}{item_pwd}{reset_vcs}{item_vcs}
-#   {item_prompt}
-#
-#   item_name, item_pwd, and item_vcs are surrounded by *_pfx and *_sfx
-
 # Theme ideas:
 #
 #  * item_name_pfx="{" item_name_sfx="}" fmt_name_pfx="38;5;42"
@@ -19,56 +12,50 @@
 
 unset fullpwd
 
-if (( havecolor )); then
-	item_name="${HOSTNAME%%.*}"
+parts[left]=":name.pfx (root):user :host :name.sfx"
+parts[right]=":vcs"
 
-	if (( UID == 0 )); then
-		item_prompt='#'
-	else
-		item_prompt='$'
-	fi
+items[:host]="${HOSTNAME%%.*}"
 
-	if [[ $USER != grawity ]]; then
-		item_name="$USER@$item_name"
-	fi
+if (( UID == 0 )); then
+	items[:prompt]='#'
+else
+	items[:prompt]='$'
 fi
+
+item_prompt=${items[:prompt]}
 
 # Some domain-based themes
 
 : ${FQDN:=$(fqdn)}
 : ${FQDN:=$HOSTNAME}
 
-parts[left]=":name.pfx (root):user (root)>@ :host :name.sfx"
-
 items[:name.pfx]=''
+items[:user.sfx]='@'
 items[:name.sfx]=''
 
 fmts[:name.root]='1;37;41'
 fmts[:name.self]='1;32'
-fmts[:name.other]='1;33'
-
 if (( UID )); then
 	fmts[:name]=@:name.self
 else
 	fmts[:name]=@:name.root
 fi
-
+fmts[:user]=@:name
 fmts[:host.pfx]=@:name.pfx
 fmts[:host]=@:name
 
 case $FQDN in
     rain.nullroute.eu.org)
 	items[:name.pfx]='┌ '
-	item_name_pfx='┌ '
-	item_prompt='┘'
-	fmt_name_pfx='|38;5;236'
-	fmt_prompt=$fmt_name_pfx
-
-	fmt_name_root='38;5;231|41'
-	fmt_name_self='38;5;82'
-	fmt_pwd='38;5;39'
-	fmt_pwd_tail='1|38;5;45'
-	fmt_vcs='38;5;198'
+	fmts[:name.pfx]='|38;5;236'
+	fmts[:name.root]='38;5;231|41'
+	fmts[:name.self]='38;5;82'
+	fmts[:pwd]='38;5;39'
+	fmts[:pwd.tail]='1|38;5;45'
+	fmts[:vcs]='38;5;198'
+	items[:prompt]='┘'
+	fmts[:prompt]=@:name.pfx
 	;;
 
     *.nullroute.eu.org)
@@ -82,64 +69,58 @@ case $FQDN in
 	    wolke)	fmts[:name.self]='|38;5;204';;
 	    *)		fmts[:name.self]='|38;5;109';;
 	esac
-	fmts[:pwd]='|2|38;5;82'
+	fmts[:pwd]='2|38;5;82'
 	fmts[:vcs]='38;5;197'
 	fullpwd=h
 	;;
 
     *.cluenet.org|*.nathan7.eu)
-	fmt_name_self='1|38;5;71'
-	fmt_pwd='38;5;144'
-	fmt_vcs='38;5;167'
+	fmts[:name.self]='1|38;5;71'
+	fmts[:pwd]='38;5;144'
+	fmts[:vcs]='38;5;167'
 	;;
 
     *.utenos-kolegija.lt)
-	item_name_pfx="["
-	item_name=$HOSTNAME
-	item_name_sfx="]"
+	items[:name.pfx]="["
+	items[:name.sfx]="]"
+	fmts[:name]=
 	if (( UID )); then
-		item_prompt='$'
-		fmt_name_pfx='|1;32'
-		fmt_name=''
+		items[:prompt]='$'
+		fmts[:name.pfx]='1;32'
 	else
-		item_prompt='#'
-		fmt_name_pfx='|1;31'
-		fmt_name='|1'
+		items[:prompt]='#'
+		fmts[:name.pfx]='1;31'
+		fmts[:name]='1'
 	fi
-	fmt_prompt=$fmt_name_pfx
 	fullpwd=y
 	;;
 
     *.core|*.rune)
-	item_name_pfx="["
-	item_name=$FQDN
-	item_name_sfx="] $OSTYPE"
-	fmt_name_pfx='|38;5;242'
-	if (( UID )); then
-		fmt_name='38;5;71'
-	else
-		fmt_name='38;5;231|41'
-	fi
-	fmt_pwd='38;5;144'
-	fmt_vcs='38;5;167'
+	items[:name.pfx]="["
+	items[:host]=$FQDN
+	items[:name.sfx]="] $OSTYPE"
+	fmts[:name.pfx]='38;5;242'
+	fmts[:name.self]='38;5;71'
+	fmts[:name.root]='38;5;231|41'
+	fmts[:pwd]='38;5;144'
+	fmts[:vcs]='38;5;167'
 	fullpwd=y
 	;;
 
     *)
-	item_name_pfx='┌ '
-	item_name=$FQDN
+	items[:name.pfx]='┌ '
+	items[:host]=$FQDN
+	items[:prompt]='└'
 	fullpwd=y
-	item_prompt='└'
-	if (( UID )); then
-		fmt_name='38;5;31'
-	else
-		fmt_name_pfx='38;5;196'
-		fmt_name='|48;5;196'
+	fmts[:name.self]='38;5;31'
+	fmts[:name.root]='48;5;196'
+	if (( ! UID )); then
+		fmts[:name.pfx]='38;5;196'
 	fi
-	fmt_pwd='38;5;76'
-	fmt_vcs='38;5;198'
-	: ${fmt_name_pfx:=$fmt_name}
-	fmt_prompt=$fmt_name_pfx
+	fmts[:pwd]='38;5;76'
+	fmts[:vcs]='38;5;198'
+	fmts[:name.pfx]=@:name
+	fmts[:prompt]=@:name.pfx
 esac
 
 # Host themes overridden in bashrc-$HOSTNAME
