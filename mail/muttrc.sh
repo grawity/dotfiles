3ceab230bc627b474dcd190a077426bea23f825a
Mantas Mikulėnas <grawity@gmail.com>
2015-12-10 08:32:39 +0200

    muttrc.sh: check for new gpg-agent socket

--- mail/muttrc.sh
+++ mail/muttrc.sh
@@ -1,27 +1,22 @@
 #!/bin/bash
 
-[[ -f ~/.msmtprc ]] &&
-cat <<!
+[[ -f ~/.msmtprc ]] && cat <<!
 set sendmail="msmtp"
 !
 
-[[ "$GPG_AGENT_INFO" ]] &&
-cat <<!
+[[ $GPG_AGENT_INFO || -S ~/.gnupg/S.gpg-agent ]] && cat <<!
 set crypt_use_gpgme
 !
 
-[[ -f "/usr/share/doc/mutt/README.Debian" ]] &&
-cat <<!
+[[ -f /usr/share/doc/mutt/README.Debian ]] && cat <<!
 set xterm_set_titles
 !
 
-[[ -d ~/.cache/mutt ]] &&
-cat <<!
+[[ -d ~/.cache/mutt ]] && cat <<!
 set header_cache="~/.cache/mutt"
 set message_cachedir="~/.cache/mutt"
 !
 
-[[ -f ~/.muttrc-"$HOSTNAME" ]] &&
-cat ~/.muttrc-"$HOSTNAME"
+[[ -f ~/.muttrc-"$HOSTNAME" ]] && cat ~/.muttrc-"$HOSTNAME"
 
 exit 0
