479300801cc41f5fbc836218932a2e807e7234bc
Mantas Mikulėnas <grawity@gmail.com>
2017-02-04 18:56:27 +0200

    bash/yank: add quoted paste

--- bash/yank.sh
+++ bash/yank.sh
@@ -4,16 +4,22 @@
 
 _xdiscard() {
 	echo -n "${READLINE_LINE:0:$READLINE_POINT}" | gclip
-	READLINE_LINE="${READLINE_LINE:$READLINE_POINT}"
+	READLINE_LINE=${READLINE_LINE:$READLINE_POINT}
 	READLINE_POINT=0
 }
 
 _xkill() {
 	echo -n "${READLINE_LINE:$READLINE_POINT}" | gclip
-	READLINE_LINE="${READLINE_LINE:0:$READLINE_POINT}"
+	READLINE_LINE=${READLINE_LINE:0:$READLINE_POINT}
 }
 
 _xyank() {
+	local str=$(pclip)
+	READLINE_LINE=${READLINE_LINE:0:$READLINE_POINT}${str}${READLINE_LINE:$READLINE_POINT}
+	READLINE_POINT=$((READLINE_POINT + ${#str}))
+}
+
+_xyankq() {
 	local str=$(pclip)
 	READLINE_LINE=${READLINE_LINE:0:$READLINE_POINT}${str}${READLINE_LINE:$READLINE_POINT}
 	READLINE_POINT=$((READLINE_POINT + ${#str}))
@@ -23,4 +29,6 @@ if have gclip && have pclip; then
 	bind -m emacs -x '"\eu": _xdiscard'
 	bind -m emacs -x '"\ek": _xkill'
 	bind -m emacs -x '"\ey": _xyank'
+	bind -m emacs -x '"\ev": _xyankq'
+	bind -m emacs -x '"\C-g": _xyankq'
 fi
