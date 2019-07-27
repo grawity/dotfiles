81e6df15894aa69a0f10e293e6e2a20aa411b753
Mantas Mikulėnas <grawity@gmail.com>
2019-07-27 17:53:59 +0300

    bash/aliases: don't check $routed6 which is set later

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -482,12 +482,10 @@ fi
 
 # shortcuts for making connections over alternate uplink
 
-if [[ $routed6 ]]; then
-	mycurl()       { do: curl --interface "$routed6" "$@"; }
-	mymtr()        { do: mtr -a "$routed6" "$@"; }
-	myping()       { do: ping -I "$routed6" "$@"; }
-	myssh()        { do: ssh -b "$routed6" "$@"; }
-	mytraceroute() { do: traceroute -s "$routed6" "$@"; }
-	mytracert()    { do: sudo tracert -s "$routed6" "$@"; }
-	mytracert6()   { do: tracert6 -s "$routed6" "$@"; }
-fi
+mycurl()       { do: curl --interface "$routed6" "$@"; }
+mymtr()        { do: mtr -a "$routed6" "$@"; }
+myping()       { do: ping -I "$routed6" "$@"; }
+myssh()        { do: ssh -b "$routed6" "$@"; }
+mytraceroute() { do: traceroute -s "$routed6" "$@"; }
+mytracert()    { do: sudo tracert -s "$routed6" "$@"; }
+mytracert6()   { do: tracert6 -s "$routed6" "$@"; }
