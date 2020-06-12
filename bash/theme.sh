96d4e1d2b7f3c5f60704b01589f94e0c76086b7b
Mantas Mikulėnas <grawity@gmail.com>
2020-06-12 12:23:00 +0300

    bash/theme: do not show ostype if it's linux

--- bash/theme.sh
+++ bash/theme.sh
@@ -83,17 +83,19 @@ case $FQDN in
 	;;
 
     *)
-	parts[left]+=" (:ostype)<:ostype"
 	items[name:pfx]="["
 	items[name:sfx]="]"
-	items[ostype]=$OSTYPE
 	fmts[name:pfx]='38;5;242'
 	fmts[name.self]='38;5;71'
 	fmts[name.root]='38;5;231|41'
-	fmts[ostype]=@name:sfx
 	fmts[pwd]='38;5;144'
 	fmts[vcs]='38;5;167'
 	fullpwd=y
+	if [[ $OSTYPE != linux-gnu ]]; then
+		parts[left]+=" (:ostype)<:ostype"
+		items[ostype]=$OSTYPE
+		fmts[ostype]=@name:sfx
+	fi
 	;;
 esac
 
