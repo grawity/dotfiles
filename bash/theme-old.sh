bad3591ba7240d1fdd7627d1aa1628077712c59c
Mantas Mikulėnas <grawity@gmail.com>
2020-06-12 12:26:24 +0300

    bash/theme: split old theme into its own file

---
+++ bash/theme-old.sh
@@ -0,0 +1,8 @@
+# The original Cn theme
+items[user:sfx]=' @ '
+if (( UID )); then
+	fmts[user:sfx]='38;5;252'
+fi
+fmts[name.self]='1|38;5;71'
+fmts[pwd]='38;5;144'
+fmts[vcs]='38;5;167'
--- bash/theme.sh
+++ bash/theme.sh
@@ -57,16 +57,6 @@ case $FQDN in
 	fi
 	;;
 
-    *.cluenet.org|*.nathan7.eu)
-	items[user:sfx]=' @ '
-	if (( UID )); then
-		fmts[user:sfx]='38;5;252'
-	fi
-	fmts[name.self]='1|38;5;71'
-	fmts[pwd]='38;5;144'
-	fmts[vcs]='38;5;167'
-	;;
-
     *.utenos-kolegija.lt)
 	items[name:pfx]="["
 	items[name:sfx]="]"
