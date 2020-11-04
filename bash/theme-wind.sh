77fe8b7bb21234a7b22c2e595cc3e1706073ff19
Mantas Mikulėnas <grawity@gmail.com>
2020-11-04 14:06:00 +0200

    bashrc: wind

--- bash/theme-wind.sh
+++ bash/theme-wind.sh
@@ -1,11 +1,10 @@
 # vim: ft=sh
 
-items[name:pfx]="-="
-items[name:sfx]="=-"
+items[name:pfx]="« "
+items[name:sfx]=" »"
 
-#fmts[name.self]="38;5;153"
-fmts[name.self]="38;5;173"
-fmts[name:pfx]="38;5;31"
+fmts[name.self]="38;5;153"
+fmts[name:pfx]="38;5;74"
 fmts[pwd]="38;5;74"
 fmts[pwd:tail]="@pwd"
 fmts[vcs]="38;5;31"
