6fa6eeec3f87d05a53c5db30a153c211b6210235
Mantas Mikulėnas <grawity@gmail.com>
2020-11-04 13:42:19 +0200

    bashrc: split themes again

---
+++ bash/theme-land.sh
@@ -0,0 +1,9 @@
+# vim: ft=sh
+
+items[name:pfx]="."
+items[name:sfx]="."
+
+fmts[name.self]="38;5;71"
+fmts[pwd]="38;5;107"
+fmts[pwd:tail]="@pwd"
+fmts[vcs]="38;5;143"
---
+++ bash/theme-sky.sh
@@ -0,0 +1,10 @@
+# vim: ft=sh
+
+items[name:pfx]="("
+items[name:sfx]=")"
+
+fmts[name.self]="38;5;146"
+fmts[name:pfx]="38;5;103"
+fmts[pwd]="38;5;218"
+fmts[pwd:tail]="@pwd"
+fmts[vcs]="38;5;216"
