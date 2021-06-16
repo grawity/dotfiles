d97eda3383cc19d58807a5921af46acb87e790d0
Mantas Mikulėnas <grawity@gmail.com>
2021-06-16 15:05:57 +0300

    theme-wind: make it gray like ember

--- bash/theme-wind.sh
+++ bash/theme-wind.sh
@@ -4,8 +4,11 @@ items[name:pfx]="{"
 items[name:sfx]="}"
 
 fmts[name.self]="38;5;109"
-fmts[pwd]="38;5;144"
-fmts[vcs]="38;5;108"
+#fmts[pwd]="38;5;144"
+#fmts[vcs]="38;5;108"
+# Grays:
+fmts[pwd]='38;5;250'
+fmts[vcs]='38;5;242'
 
 fmts[name:pfx]="@name"
 fmts[pwd:tail]="@pwd"
