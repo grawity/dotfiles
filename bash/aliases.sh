045de43db387070af9ab61d443d21f51b8559c61
Mantas Mikulėnas <grawity@gmail.com>
2021-12-14 14:33:59 +0200

    bash: fzf: sort at least the non-recursive lists
    For some reason, compgen sorts output when bash is interactive, but not
    when it's being run via bash -c?

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -528,8 +528,8 @@ if have fzf; then
 		fi
 	}
 	# Alt+[df] - local dir/all selection
-	bind -m emacs -x '"\ed": _fzfyank "compgen -d"'
-	bind -m emacs -x '"\ef": _fzfyank "compgen -f"'
+	bind -m emacs -x '"\ed": _fzfyank "compgen -d | sort"'
+	bind -m emacs -x '"\ef": _fzfyank "compgen -f | sort"'
 	# Alt+Shift+[DF] - recursive dir/all selection
 	bind -m emacs -x '"\eD": _fzfyank "find . -xdev -mindepth 1 -name .\?\* -prune -o -type d -printf %P\\\n"'
 	bind -m emacs -x '"\eF": _fzfyank "find . -xdev -mindepth 1 -name .\?\* -prune -o -printf %P\\\n"'
