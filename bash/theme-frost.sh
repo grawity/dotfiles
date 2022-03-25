826d3bf4c5143cddb33d50a21d99f4596e1bef45
Mantas Mikulėnas <grawity@gmail.com>
2022-03-25 13:05:28 +0200

    bash: tweak frost prompt
    ┌ grawity on frost [~/.dotfiles/bash] master
    └ _

--- bash/theme-frost.sh
+++ bash/theme-frost.sh
@@ -1,15 +1,27 @@
+bright="38;5;153"
+normal="38;5;110"
+
 items[name:pfx]='┌ '
-fmts[name:pfx]='38;5;240'
-fmts[name.root]='38;5;231|41'
-fmts[name.self]='38;5;109|1'
-fmts[pwd]='38;5;109'
-fmts[pwd:tail]='38;5;152'
-fmts[vcs]='38;5;174'
 items[prompt]='└'
+fmts[name:pfx]='38;5;240'
 fmts[prompt]=@name:pfx
+
+fmts[name.self]="$normal"
+fmts[host]="$bright"
+fmts[pwd]="$normal"
+fmts[pwd:tail]="$bright"
+fmts[vcs]="38;5;174"
+
+items[pwd:head:pfx]='['
+items[pwd:tail:sfx]=']'
+fmts[pwd:head:pfx]=@name:pfx
+fmts[pwd:tail:sfx]=@pwd:head:pfx
+#fullpwd=y
 
 # Show username even for non-root
 items[user.self]=y
 items[user:sfx]=' on '
 fmts[user:sfx]=@prompt
-fmts[host]=@name.self
+
+unset bright
+unset normal
