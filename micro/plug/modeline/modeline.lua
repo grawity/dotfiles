0f70fdeced5cf4b4379e8d07a29d12d2ca1f2772
Mantas Mikulėnas <grawity@gmail.com>
2022-05-09 12:40:16 +0300

    micro: fix bool modeline parameters

--- micro/plug/modeline/modeline.lua
+++ micro/plug/modeline/modeline.lua
@@ -3,30 +3,42 @@ local micro = import("micro")
 local function setFileType(buf, ft)
 	if ft == "sh" then
 		buf:SetOption("filetype", "shell")
+	else
+		micro.Log("Unsupported Vim filetype '"..ft.."'")
 	end
 end
 
 local function handleModeline(buf, line)
-	micro.Log("it's a modeline: <" .. line .. ">")
+	micro.Log("Found Vim modeline <"..line..">")
 	for t in line:gmatch("[^:]+") do
-		micro.Log("token <" .. t .. ">")
 		k, v = t:match("([^=]+)=(.+)")
 		if k and v then
-			micro.Log("  token is kvpair <" .. k .. "> = <" .. v .. ">")
+			micro.Log("Modeline option <"..k.."> value <"..v..">")
 			if k == "ft" then
 				setFileType(buf, v)
 			elseif k == "ts" then
 				buf:SetOption("tabsize", v)
+			elseif k == "sw" then
+				-- ignore
+			else
+				micro.Log("Unknown Vim modeline option '"..k.."'")
 			end
 		else
-			micro.Log("  token is bool <" .. t .. ">")
-			if k == "et" then
-				buf:SetOption("tabstospaces", true)
-			elseif k == "noet" then
-				buf:SetOption("tabstospaces", false)
+			micro.Log("Modeline option <"..t.."> bool")
+			if t == "et" then
+				buf:SetOption("tabstospaces", "on")
+			elseif t == "noet" then
+				buf:SetOption("tabstospaces", "off")
+			elseif t == "wrap" then
+				buf:SetOption("softwrap", "on")
+			elseif t == "nowrap" then
+				buf:SetOption("softwrap", "off")
+			else
+				micro.Log("Unknown Vim modeline option '"..t.."'")
 			end
 		end
 	end
+	micro.Log("Finished modeline processing")
 end
 
 function onBufferOpen(buf)
@@ -36,14 +48,13 @@ function onBufferOpen(buf)
 
 	local ft = buf:FileType()
 	if (ft ~= nil) and (ft ~= "unknown") then
-		micro.Log("filetype not nil: " .. ft)
+		micro.Log("Not looking for modeline (already have filetype '"..ft.."')")
 		return
 	end
 
 	local i
 	for i = 0, 3 do
 		local line = buf:Line(i)
-		micro.Log("line " .. i .. " is <" .. line .. ">")
 		local match = line:match(" vim?: ([^ ]+)")
 		if match then
 			return handleModeline(buf, match)
