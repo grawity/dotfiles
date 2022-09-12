29d957200edcd38331f01ff579d7f867745bc06e
Mantas Mikulėnas <grawity@gmail.com>
2022-09-12 11:42:16 +0300

    bash: do something for basic $PS1 on dumb terminals

--- bash/basicprompt.sh
+++ bash/basicprompt.sh
@@ -1,12 +1,18 @@
-# PS1 used on gw-core that turned out to be nice
+# Basic prompt for VMs or old terminals
 
-if [[ $TERM == *-256color ]]; then
+if [[ $TERM == *-@(256color|direct) ]]; then
 	if (( UID > 0 )); then
 		PS1='\[\e[m\e[38;5;138m\]\u@\h \[\e[38;5;103m\]$PWD \[\e[38;5;244m\]\$\[\e[m\] '
 	elif [[ $HOSTNAME == vm-vol* ]]; then
 		PS1='\[\e[m\e[38;5;208m\]\u@\h \[\e[38;5;103m\]$PWD \[\e[38;5;244m\]\$\[\e[m\] '
 	else
 		PS1='\[\e[m\e[38;5;203m\]\u@\h \[\e[38;5;103m\]$PWD \[\e[38;5;244m\]\$\[\e[m\] '
+	fi
+else
+	if (( UID > 0 )); then
+		PS1='\[\e[m\e[1m\](\u@\h \W)\$\[\e[m\] '
+	else
+		PS1='\[\e[m\e[1;31m\](\u@\h \W)\$\[\e[m\] '
 	fi
 fi
 
