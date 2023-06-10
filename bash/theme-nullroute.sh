2aa6b91f4b53643e2b2cab066ce1c488b63cbe01
Mantas Mikulėnas <grawity@gmail.com>
2023-06-10 17:31:18 +0300

    bash: trim down the default theme to bare minimum

--- bash/theme-nullroute.sh
+++ bash/theme-nullroute.sh
@@ -1,16 +1,18 @@
 # fallback theme for those *.nullroute.lt hosts which DO NOT have their own theme
-items[name:pfx]='['
-items[name:sfx]=']'
-fmts[name:pfx]='38;5;66'
-fmts[name.root]='38;5;220'
-case $HOSTNAME in
-    sky)	fmts[name.self]='38;5;43';;
-    star)	fmts[name.self]='38;5;208';;
-    river)	fmts[name.self]='38;5;33';;
-    wolke)	fmts[name.self]='38;5;204';;
-    *)		fmts[name.self]='38;5;109';;
-esac
-fmts[pwd]='38;5;82'
-fmts[pwd:tail]='1|38;5;82'
-fmts[vcs]='38;5;197'
+#items[name:pfx]='['
+#items[name:sfx]=']'
+#fmts[name:pfx]='38;5;66'
+fmts[name.root]='1;31'
+fmts[name.self]='1'
+#case $HOSTNAME in
+#    sky)	fmts[name.self]='38;5;43';;
+#    star)	fmts[name.self]='38;5;208';;
+#    river)	fmts[name.self]='38;5;33';;
+#    wolke)	fmts[name.self]='38;5;204';;
+#    *)		fmts[name.self]='38;5;109';;
+#esac
+#fmts[pwd]='38;5;82'
+#fmts[pwd:tail]='1|38;5;82'
+#fmts[vcs]='38;5;197'
+fmts[vcs]='33'
 fullpwd=h
