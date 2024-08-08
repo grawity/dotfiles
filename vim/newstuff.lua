b9f51b4e6716a75daa371766cfaafe6240eed68f
Mantas Mikulėnas <grawity@gmail.com>
2024-08-08 12:37:32 +0300

    vim: try installing obsidian.nvim

--- vim/newstuff.lua
+++ vim/newstuff.lua
@@ -1,2 +1,27 @@
 require("lazy").setup({
+	spec = {
+		{
+			"epwalsh/obsidian.nvim",
+			version = "*",
+			--lazy = true,
+			--ft = "markdown",
+			dependencies = {
+				"nvim-lua/plenary.nvim",
+			},
+			config = function()
+				require("obsidian").setup({
+					ui = {
+						-- disable fancy features that require conceallevel
+						enable = false,
+					},
+					workspaces = {
+						{
+							name = "personal",
+							path = "~/test",
+						},
+					},
+				})
+			end,
+		},
+	},
 })
